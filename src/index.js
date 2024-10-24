99c95a84154cc157c621ca1a42bb4f5d1cccfa42
gena230295 <95904840+gena230295@users.noreply.github.com>
2024-10-24 14:54:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  /*str = "|()|(||)||";
+  bracketsConfig = [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+    ["|", "|"],
+  ];*/
+
+  const newStr = bracketsConfig.join("");
+  let stillStr = "";
+
+  for (i = 0; i < newStr.length; i++) {
+    if (newStr[i] == ",") {
+      stillStr += "";
+    } else {
+      stillStr += newStr[i];
+    }
+  }
+
+  let newArr = [];
+  for (i = 0; i < stillStr.length; i = i + 2) {
+    newArr.push(stillStr[i] + stillStr[i + 1]);
+  }
+
+  let stillNewStr = str;
+
+  for (let i = 0; i < str.length; i++) {
+    newArr.forEach((j) => {
+      stillNewStr = stillNewStr.replace(j, "");
+    });
+  }
+  if (stillNewStr.length == 0) {
+    return true;
+  }
+  if (stillNewStr.length != 0) {
+    return false;
+  }
+
+//   console.log(str);
+//   console.log(newArr);
+//   console.log(stillNewStr);
 }
+// console.log(check());
